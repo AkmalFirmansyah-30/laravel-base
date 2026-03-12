3d9432fe5544fe5d755c9600f5271d748c82ae34
AkmalFirmansyah-30 <firmansyahakmal289@gmail.com>
2026-03-12 09:32:51 +0700

    Week 2

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/belajar-route',function(){
+    return "Selamat Datang Di Halaman Belajar-Route";
+});
+
+Route::match(['get','post'], 'belajar-route', function(){
+    return "Ini Halaman belajar-route";
+});
+
+Route::redirect('route','new-route');
+
+Route::get('new-route', function() {
+    return "Ini Halaman Route Baru";
+});
+
+Route::get('/belajar-route/{page}', function($page){
+    return "Hello, Ini Halaman Belajar Route $page";
+});
